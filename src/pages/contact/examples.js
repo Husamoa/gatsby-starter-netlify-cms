36a1f8c2f1b0c91973d060c607443eb1bca119c9
Husamoa <pawebial@gmail.com>
2019-05-28 20:59:39 +0200

    add iframe movie from fb

--- src/pages/contact/examples.js
+++ src/pages/contact/examples.js
@@ -22,9 +22,12 @@ export default class Index extends React.Component {
                 </li>
                 <li>
                   tel: <a href="tel:+48668336781">668 336 781</a>
-
                 </li>
               </ul>
+              <h4>Zapraszam do obejrzenia spotu promującego Tenis Stołowy Dźwiękowy</h4>
+              <div className="responsive-container">
+              <iframe className="responsive-iframe" title="Spot Tenisa Stołowego Dźwiękowego" src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fdzwiekowy%2Fvideos%2F363264254438433%2F&show_text=0&width=560" allowFullScreen={true}></iframe>
+              </div>
             </div>
           </div>
         </section>
