2f131735a4aebc0134031209c4b1b70829ee9fa8
Husamoa <pawebial@gmail.com>
2019-11-02 10:51:30 +0100

    added foundation.js

--- src/pages/fundation.js
+++ src/pages/fundation.js
@@ -3,10 +3,19 @@ import Layout from '../components/Layout'
 
 const Foundation = () => (
   <Layout>
-    <div className="not-found-page">
-      <h1>Cierpliwości...</h1>
-      <p>Fundacja jest właśnie na etapie rejestracji w KRS.</p>
-    </div>
+    <section className="section section-medium-padding">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <div className="section">
+              <img src="../img/og-image.jpg"/>
+              <h1>Cierpliwości...</h1>
+              <p>Fundacja jest właśnie na etapie rejestracji w KRS.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </Layout>
 )
 
