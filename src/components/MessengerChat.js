0b85c705e38716a5383a577ad846555110c84f1b
Husamoa <pawebial@gmail.com>
2019-11-03 11:44:56 +0100

    greetingDialogDisplay show

--- src/components/MessengerChat.js
+++ src/components/MessengerChat.js
@@ -5,9 +5,10 @@ const MessengerChat = () => (
   <MessengerCustomerChat
     pageId="647906501936249"
     appId="706255716535048"
-    themeColor="#67b868"
     loggedInGreeting="Witaj na czacie Tenisa Stołowego Dźwiękowego :) W czym możemy pomóc?"
     loggedOutGreeting="Witaj na czacie Tenisa Stołowego Dźwiękowego :) W czym możemy pomóc?"
+    greetingDialogDisplay="show"
+
   />
 )
 
