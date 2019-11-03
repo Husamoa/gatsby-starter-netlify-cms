abea63b8dedcb2a70e3e8ff1ae063b162f535082
Husamoa <pawebial@gmail.com>
2019-11-03 11:18:57 +0100

    deleted message delay

--- src/components/MessengerChat.js
+++ src/components/MessengerChat.js
@@ -5,38 +5,8 @@ const MessengerChat = () => (
   <MessengerCustomerChat
     pageId="647906501936249"
     appId="706255716535048"
-    greetingDialogDisplay="Witamy na czacie Tenisa Stołowego Dźwiękowego :) W czym możemy pomóc?"
     themeColor="#67b868"
   />
 )
 
 export default MessengerChat
-
-/*{
-  <!-- Load Facebook SDK for JavaScript -->
-      <div id="fb-root"></div>
-      <script>
-        window.fbAsyncInit = function() {
-          FB.init({
-            xfbml            : true,
-            version          : 'v5.0'
-          });
-        };
-
-        (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/pl_PL/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));</script>
-
-      <!-- Your customer chat code -->
-      <div class="fb-customerchat"
-        attribution=setup_tool
-        page_id="647906501936249"
-  theme_color="#67b868"
-  logged_in_greeting="Witamy na czacie Tenisa Stołowego Dźwiękowego :) W czym możemy pomóc?"
-  logged_out_greeting="Witamy na czacie Tenisa Stołowego Dźwiękowego :) W czym możemy pomóc?">
-      </div>
-}*/
