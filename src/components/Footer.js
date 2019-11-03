c35fe6a6df9ed0b859ca8434492fe2402768fcb5
Husamoa <pawebial@gmail.com>
2019-11-03 20:01:40 +0100

    change footer facebook

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import logo from '../img/logo-tsd.jpg'
-import facebook from '../img/social/facebook.svg'
-import instagram from '../img/social/instagram.svg'
-import twitter from '../img/social/twitter.svg'
-import vimeo from '../img/social/vimeo.svg'
+import facebookWhite from '../img/social/facebook1.svg'
 
 const Footer = class extends React.Component {
   render() {
@@ -64,39 +61,20 @@ const Footer = class extends React.Component {
                         Kontakt
                       </Link>
                     </li>
+                    <li>
+                      <a title="facebook" href="https://www.facebook.com/dzwiekowy/">
+                        <img
+                          src={facebookWhite}
+                          alt="Facebook"
+                          style={{ width: '1em', height: '1em' }}
+                        />
+                      </a>
+                    </li>
                   </ul>
                 </section>
               </div>
               <div className="column is-4 social">
-                <a title="facebook" href="https://www.facebook.com/dzwiekowy/">
-                  <img
-                    src={facebook}
-                    alt="Facebook"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="twitter" href="https://twitter.com">
-                  <img
-                    className="fas fa-lg"
-                    src={twitter}
-                    alt="Twitter"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="instagram" href="https://instagram.com">
-                  <img
-                    src={instagram}
-                    alt="Instagram"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="vimeo" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
+
               </div>
             </div>
           </div>
